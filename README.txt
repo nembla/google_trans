4730d3ef2ba04e29fea3a6ce695e0ef3a5b505e7
Nemanja Blagojevic <zzirnemus@gmail.com>
2021-04-19 17:49:54 -0400

    swapped from entry to text, added proper padding and sizing of boxes, changed output state after result was outputted to avoid editing of translated content.

---
+++ README.txt
@@ -0,0 +1,10 @@
+This is a personal project I have coded from scratch, 
+as I love programming as well as learning new languages. 
+
+It relies on an alpha version of google translate python 
+package, and tkinter for the GUI. This is my first project
+using tkinter and I think its going well so far. 
+
+Future updates:
+Adding support for all support google translate languages
+Cleaning up the GUI and adding clear input text button
--- main.py
+++ main.py
@@ -8,13 +8,16 @@ from googletrans import Translator
 #initialize translator and set geometry for window
 translator = Translator()
 root = Tk()
-root.geometry("500x400")
+root.geometry("500x500")
 root.title("Language Translator")
 
 #google translation
 def gtrans():
+    #sets output text state to enabled
+    output_entry.configure(state='normal')
+
     #gets the content to be translated
-    get_text = input_entry.get()
+    get_text = input_entry.get("1.0", END)
     in_lang = translator.detect(get_text)    
 
     #gets the target language and swaps string to google translate keyphrase for lang selection  
@@ -32,8 +35,12 @@ def gtrans():
 
     #translates and inserts result into output_entry      
     result = translator.translate(text=get_text, dest=dest, src=in_lang.lang)
-    output_entry.delete(0, "end")
-    output_entry.insert(0, result.text)
+    output_entry.delete("1.0", END)
+    output_entry.insert("1.0", result.text)
+
+    #sets output text state to disabled to avoid editing
+    output_entry.configure(state='disabled')
+
     return
  
 #Target Language Drop Down Menu
@@ -42,31 +49,31 @@ variable = StringVar(root)
 variable.set(lang_options[0])                                               #default value is spanish
 
 target_language = Label(root, text="Select Target Language: ")
-target_language.grid(row=0, column=0)
+target_language.grid(row=0, column=0, padx="20", pady="25")
 
 options = OptionMenu(root, variable, *lang_options)
-options.grid(row=0,column=1)
+options.grid(row=0,column=2)
 
 #input label and entry boxes
-input_label = Label(root, text="Input translation text:")
-input_label.grid(row=1, column=0)
+input_label = Label(root, text="Input translation text:\n(in any language)", pady="25")
+input_label.grid(row=1, column=0, pady="20")
 
-input_entry = Entry(fg="black", bg="white", highlightbackground="black", highlightthickness="1")
-input_entry.grid(row=1,column=1)
+input_entry = Text(fg="black", bg="white", highlightbackground="black", highlightthickness="1", height="8", width="22") #swapped from entry to Text
+input_entry.grid(row=1,column=2, pady="20")
 
 #result label and entry boxes
-result_label = Label(root, text="Translated text:")
-result_label.grid(row=2, column=0)
-output_entry = Entry(fg="black", bg="white", highlightbackground="black", highlightthickness="1" )
-output_entry.grid(row=2,column=1)
+result_label = Label(root, text="Translated text:", pady="25")
+result_label.grid(row=2, column=0, pady="20")
+output_entry = Text(fg="black", bg="white", highlightbackground="black", highlightthickness="1", height="8", width="22") #swapped from entry to text
+output_entry.grid(row=2,column=2, pady="20")
 
 #green translate button
 translate_button = Button(text="Translate", bg="lightgreen", fg="black", command=gtrans)
-translate_button.grid(row=3,column=0)
+translate_button.grid(row=3,column=1, pady="8", padx="10")
 
 #quit button
 quit_button = Button(text="Quit", command=root.destroy)
-quit_button.grid(row=4, column=0)
+quit_button.grid(row=4, column=1)
 
 ##main loop##
 root.mainloop()
