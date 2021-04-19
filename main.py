c0b15fc6aca5e32e308a1e19baf401b7bf5e8a6b
Nemanja Blagojevic <zzirnemus@gmail.com>
2021-04-19 00:10:10 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,36 @@
+#Language translator from English to any target using google translate requests
+#Currently supports: Spanish (es), Afrikaans (af), and Norwegian (no)
+#Support can be expanded in future project for practice. 
+from googletrans import Translator
+
+
+translator = Translator()
+run = True
+
+#Language Selection
+while run == True:
+    print("\n" + "Welcome to my English Language Translator. Select from the following menu:")
+    print("1.Spanish\n2.Afrikaans\n3.Norwegian")
+    menu = int(input("Enter target langauge: "))
+
+    if menu == 1:
+        print("Spanish selected.")
+        target_language = 'es'
+
+    elif menu == 2:
+        print("Afrikaans selected.")
+        target_language = 'af'
+
+    elif menu == 3:
+        print("Norwegian selected.")
+        target_language = 'no'
+
+    else:
+        print("Error: no language selected.")
+        continue
+    run = False
+
+content = input("Enter content to be translated: ")                                         #content to be translated
+in_lang = translator.detect(content)                                                        #detect input language
+result = translator.translate(text=content, dest=target_language, src=in_lang.lang)         #translate input to target language
+print(result.text)                                                                          #output results
